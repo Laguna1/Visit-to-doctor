bff8f7a4e5f4a2681511c41749e666044a8ef320
Laguna1 <lagunazoomarket@gmail.com>
2021-11-18 01:29:35 +0200

    Define scopes for past&upcoming visits

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,4 +2,7 @@ class Visit < ApplicationRecord
   belongs_to :pat, class_name: 'User'
   has_one :appointment, dependent: :destroy
   has_one :doc, through: :appointments, source: :user
+
+  scope :past_visits, -> { where('date < ?', Date.today) }
+  scope :upcoming_visits, -> { where('date >= ?', Date.today) }
 end
