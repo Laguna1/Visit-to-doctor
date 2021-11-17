62e5faf93c983563d53f4c39f039cc575de33e2e
Laguna1 <lagunazoomarket@gmail.com>
2021-11-17 21:33:35 +0200

    Add for user the ability to log in using a mobile number instead of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Search user by mobile number(not email)
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    where(mobile_no: conditions[:mobile_no]).first
+  end
+
+  # Stop using email as authentication key
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 end
