b8a55fbfce843f37fd3b4326bb6cfe1c4dab13ea
Laguna1 <lagunazoomarket@gmail.com>
2021-11-20 00:09:49 +0200

    Seede users with categories & fix users list view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# User.destroy.all 
+# User.destroy.all
+# Category.destroy.all
+
 # u1 = User.create({ fullname: 'Sally Major', mobile_no: '380501234567', email: 'sally@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa' })
 # u2 = User.create({ fullname: 'Sue Kostner', mobile_no: '380661111111', email: 'sue@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa' })
 # u3 = User.create({ fullname: 'Kevin Spacy', mobile_no: '380672222222', email: 'kevin@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa' })
-# u4 = User.create({ fullname: 'Jack Nortingen', mobile_no: '380733333333', email: 'jack@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa' })
+u4 = User.create({ fullname: 'Kurt Backham', mobile_no: '380681111111', email: 'kurt@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', category_id: 1 })
+u5 = User.create({ fullname: 'Jack Nortingen', mobile_no: '380691111111', email: 'jack@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', category_id: 1 })
+u6 = User.create({ fullname: 'Eddy Murphy', mobile_no: '380700000000', email: 'eddy@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', category_id: 1 })
+u7 = User.create({ fullname: 'Jackob Wolfskin', mobile_no: '380710101010', email: 'jackob@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', category_id: 2 })
+u8 = User.create({ fullname: 'Mila Pristley', mobile_no: '380721212121', email: 'mila@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', category_id: 2 })
 
-c1 = Category.create(speciality: 'Therapist')
-c2 = Category.create(speciality: 'Surgeon')
-c3 = Category.create(speciality: 'Pediatrician')
-c4 = Category.create(speciality: 'Gynecologist')
-c5 = Category.create(speciality: 'Dentist')
-c6 = Category.create(speciality: 'Dermatologist')
-c7 = Category.create(speciality: 'Ophthalmologist')
-c8 = Category.create(speciality: 'Urologist')
-c9 = Category.create(speciality: 'Psychiatrist')
+
+# c1 = Category.create(speciality: 'Therapist')
+# c2 = Category.create(speciality: 'Surgeon')
+# c3 = Category.create(speciality: 'Pediatrician')
+# c4 = Category.create(speciality: 'Gynecologist')
+# c5 = Category.create(speciality: 'Dentist')
+# c6 = Category.create(speciality: 'Dermatologist')
+# c7 = Category.create(speciality: 'Ophthalmologist')
+# c8 = Category.create(speciality: 'Urologist')
+# c9 = Category.create(speciality: 'Psychiatrist')
