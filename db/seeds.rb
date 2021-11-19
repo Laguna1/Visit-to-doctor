9fd9d2e622c5e311391bedd548ce53093601373a
Laguna1 <lagunazoomarket@gmail.com>
2021-11-19 23:44:17 +0200

    Seeds categories to database, modify category views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy.all 
-u1 = User.create({ fullname: 'Sally Major', mobile_no: '380501234567', email: 'sally@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa' })
-u2 = User.create({ fullname: 'Sue Kostner', mobile_no: '380661111111', email: 'sue@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa' })
-u3 = User.create({ fullname: 'Kevin Spacy', mobile_no: '380672222222', email: 'kevin@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa' })
-u4 = User.create({ fullname: 'Jack Nortingen', mobile_no: '380733333333', email: 'jack@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa' })
+# User.destroy.all 
+# u1 = User.create({ fullname: 'Sally Major', mobile_no: '380501234567', email: 'sally@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa' })
+# u2 = User.create({ fullname: 'Sue Kostner', mobile_no: '380661111111', email: 'sue@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa' })
+# u3 = User.create({ fullname: 'Kevin Spacy', mobile_no: '380672222222', email: 'kevin@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa' })
+# u4 = User.create({ fullname: 'Jack Nortingen', mobile_no: '380733333333', email: 'jack@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa' })
+
+c1 = Category.create(speciality: 'Therapist')
+c2 = Category.create(speciality: 'Surgeon')
+c3 = Category.create(speciality: 'Pediatrician')
+c4 = Category.create(speciality: 'Gynecologist')
+c5 = Category.create(speciality: 'Dentist')
+c6 = Category.create(speciality: 'Dermatologist')
+c7 = Category.create(speciality: 'Ophthalmologist')
+c8 = Category.create(speciality: 'Urologist')
+c9 = Category.create(speciality: 'Psychiatrist')
